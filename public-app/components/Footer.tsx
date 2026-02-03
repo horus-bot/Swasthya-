22f0f049534cbe09925d35650990396aba4469eb
Gokul-Krishana <gokulkrishana640@gmail.com>
2026-02-04 00:00:44 +0530

    pushing the image component

---
+++ public-app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 text-center text-xs p-3 mt-6">
+      © Government Public Healthcare Initiative
+    </footer>
+  );
+}
